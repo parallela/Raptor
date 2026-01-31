9747bc57809ac1551ecf576f6e85ffe7d92ce4b7
lubomirstankov <lubomir.c.stankov@gmail.com>
2026-02-01 00:46:59 +0200

    feat: add tty column to flakes table for interactive container support

--- migrations/20260201000001_flake_tty_fix.sql
+++ migrations/20260201000001_flake_tty_fix.sql
@@ -1,10 +1,14 @@
--- Fix tty column: ensure NOT NULL constraint and default value
--- This fixes the previous migration that was modified
+-- Add tty column to flakes table for interactive container support
+-- Some games like Hytale require a TTY for authentication
+-- Default is false for cleaner logs
+
+-- Add the column if it doesn't exist
+ALTER TABLE flakes ADD COLUMN IF NOT EXISTS tty BOOLEAN DEFAULT false;
 
 -- Update any NULL values to false
 UPDATE flakes SET tty = false WHERE tty IS NULL;
 
--- Alter column to be NOT NULL with default false (if not already)
+-- Set NOT NULL constraint
 ALTER TABLE flakes ALTER COLUMN tty SET NOT NULL;
 ALTER TABLE flakes ALTER COLUMN tty SET DEFAULT false;
 
